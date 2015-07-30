86baf832534a0235a5ebcecd6e6b24c8e1fbdeda
Jono Mingard <reason.koan@gmail.com>
2015-07-31 00:06:05 +1200

    Add skeleton test classes

---
+++ src/test/GameDataTests.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameDataTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
